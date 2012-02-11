8af73e7680eed31de099c7ee192a38ea470ae3bb
Luiz Fonseca <runeroniek@gmail.com>
2012-02-11 20:35:47 -0200

    Remove Slim logic style from app

--- config/environment.rb
+++ config/environment.rb
@@ -1,6 +1,5 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 
-Slim::Engine.set_default_options :sections => true
 # Initialize the rails application
 MateriaBrasil::Application.initialize!
