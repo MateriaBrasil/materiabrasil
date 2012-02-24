80d08e61981478ae243ba7e78e38226320233c97
Luiz Fonseca <runeroniek@gmail.com>
2012-02-24 15:23:20 -0200

    Add Google Analytics tracking code

--- app/assets/javascripts/frontend/app.js
+++ app/assets/javascripts/frontend/app.js
@@ -19,6 +19,17 @@ document.getElementById('mailee-form').onsubmit = function(){
 };
 
 
+var _gaq = _gaq || [];
+_gaq.push(['_setAccount', 'UA-29468390-1']);
+_gaq.push(['_trackPageview']);
+
+(function() {
+  var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+  ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+  var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+})();
+
+
 var App = window.App = {
 
   init: function(){
