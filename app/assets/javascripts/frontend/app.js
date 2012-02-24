adafd32ffd83008ddf9bb256c2f808bdd808bf54
Luiz Fonseca <runeroniek@gmail.com>
2012-02-24 11:52:33 -0200

    Insert text for landing page and fix twitter/mailee and the logo of the partners

--- app/assets/javascripts/frontend/app.js
+++ app/assets/javascripts/frontend/app.js
@@ -8,6 +8,17 @@
 
 (function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="//platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs"));
 
+document.getElementById('mailee-form').onsubmit = function(){
+  re = /^[a-z0-9\._-]+@([a-z0-9][a-z0-9-_]*[a-z0-9-_]\.)+([a-z-_]+\.)?([a-z-_]+)$/
+    if(!this.email.value.match(re)) {
+      alert("Por favor, preencha corretamente o email");
+      this.email.focus();
+      return false;
+    }
+  return true;
+};
+
+
 var App = window.App = {
 
   init: function(){
@@ -15,7 +26,7 @@ var App = window.App = {
   },
 
   getTwitterStatus: function(){
-    $.getJSON("https://twitter.com/statuses/user_timeline/fibra_design.json?callback=?", function(data){
+    $.getJSON("https://twitter.com/statuses/user_timeline/materiabrasil.json?callback=?", function(data){
       $(".networks .twitter blockquote").html(data[0].text);
     });
   }
