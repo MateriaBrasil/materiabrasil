97ab84a52f4fa5a082afc0ee7209e7d79608d485
Luiz Fonseca <runeroniek@gmail.com>
2012-03-25 15:56:47 -0300

    use form partial to handle material insertions and stuff.

--- app/admin/materials.rb
+++ app/admin/materials.rb
@@ -1,24 +1,5 @@
 # coding: utf-8
 ActiveAdmin.register Material do
 
-  form do |f|
-    f.inputs "Categorias" do
-      f.input :categories, :as => :check_boxes, :label => "Categorias"
-    end
-    f.inputs "Sobre o material" do
-      f.input :resume, :label => "Resumo"
-    end
-
-    f.inputs "Informações técnicas" do
-      f.input :technical_observation, :label => "Observações Técnicas"
-    end
-
-    f.inputs "Detalhes técnicos" do
-      f.input :density, :label => "Densidade"
-      f.input :dimensions, :label => "Dimensões"
-      f.input :packing, :label => "Embalagem"
-      f.input :average_price, :label => "Preço Médio"
-    end
-    f.buttons
-  end
+  form :partial => "form"
 end
