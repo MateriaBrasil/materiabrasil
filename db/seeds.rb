d73b94ed2f222d86f0aa29f0ecba7ca1258cf939
Luiz Fonseca <runeroniek@gmail.com>
2012-04-08 19:06:45 -0300

    Change seeds.

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,24 +7,4 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 #
-AdminUser.create!(:email => "runeroniek@gmail.com", :password => "skeeter")
-
-
-
-Category.create!([
-  {:name => "Classes"},
-    { :name => "Cerâmico", :parent_id => 1, :code_reference => 1},
-    { :name => "Couro", :parent_id => 1, :code_reference => 20},
-    { :name => "Fibras Naturais", :parent_id => 1, :code_reference => 40},
-    { :name => "Madeira", :parent_id => 1, :code_reference => 60},
-    { :name => "Metal", :parent_id => 1, :code_reference => 80},
-    { :name => "Papel e celulose", :parent_id => 1, :code_reference => 100},
-    { :name => "Polímero", :parent_id => 1, :code_reference => 120},
-    { :name => "Florestal não-madeireiro", :parent_id => 1, :code_reference => 140},
-    { :name => "Vidro", :parent_id => 1, :code_reference => 160},
-    { :name => "Compósito", :parent_id => 1, :code_reference => 180},
-  {:name => "Usos"},
-    { :name => "Acabamento tintas/vernizes", :parent_id => 12, :code_reference => 500},
-    { :name => "Acessórias", :parent_id => 12, :code_reference => 520},
-    { :name => "Adesivo", :parent_id => 12, :code_reference => 540}
-])
+AdminUser.create!(:email => "test@test.com", :password => "test")
