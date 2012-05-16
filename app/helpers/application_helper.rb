38fa8062dcb09c72503a94434e227cd40e983e6f
Luiz Fonseca <runeroniek@gmail.com>
2012-05-15 22:58:15 -0300

    Try to fix ruby bug.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
 
   def load_block(options = {})
-    block = options[:block] || rand(1..6).to_i
+    block = options[:block] || rand(1..5) + 1
     material = options[:materials]
     blocks = {
       1 => [ 
