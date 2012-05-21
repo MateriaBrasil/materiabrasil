19e76daea3b82650869ed1f637b67121a3d9e4f6
Luiz Fonseca <runeroniek@gmail.com>
2012-05-21 11:31:54 -0300

    Change initializer and remove some not-used configurations.

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -16,7 +16,7 @@ class ImageUploader < CarrierWave::Uploader::Base
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
-    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+    "uploads/#{mounted_as}_#{model.id}"
   end
 
   process :resize_to_fill => [800, 600]
@@ -57,8 +57,8 @@ class ImageUploader < CarrierWave::Uploader::Base
 
   # Override the filename of the uploaded files:
   # Avoid using model.id or version_name here, see uploader/store.rb for details.
-  # def filename
-  #   "something.jpg" if original_filename
-  # end
-
+  if original_filename 
+    @name ||= Digest::MD5.hexdigest(File.dirname(current_path))
+    "#{@name}.#{file.extension}"
+  end
 end
--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -5,7 +5,5 @@ CarrierWave.configure do |config|
     :aws_secret_access_key  => ENV['AMAZON_S3_SECRET'],       # required
   }
   config.fog_directory  = 'materiabrasil'                         # required
-  config.fog_host       = 'https://assets.materiabrasil.com'      # optional, defaults to nil
-  config.fog_public     = false                                   # optional, defaults to true
   config.fog_attributes = {'Cache-Control'=>'max-age=315576000'}  # optional, defaults to {}
 end
