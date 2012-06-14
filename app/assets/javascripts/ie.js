be40f23c8cab1144413419825af2b1ff769a6f42
Luiz Fonseca <runeroniek@gmail.com>
2012-06-14 10:22:40 -0300

    Remove old files and .swp files.

--- app/assets/javascripts/ie.js
+++
@@ -1,3 +0,0 @@
-// Just need this to make sure we're dealing with good markup :)
-var tags = ['header', 'article', 'section', 'footer'];
-for (var i = 0; i < tags.length; i++) { document.createElement(tags[i]) }
