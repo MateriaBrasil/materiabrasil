d6f3d95878aa9e37f683089cf2cb235368a80642
Luiz Fonseca <runeroniek@gmail.com>
2012-09-09 19:49:24 -0300

    Trying to fix

--- config/application.rb
+++ config/application.rb
@@ -55,7 +55,7 @@ module MateriaBrasil
     config.assets.initialize_on_precompile = false
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
-
+    config.assets.paths << Rails.root.join("app", "assets", "fonts")
 
     config.generators do |g|
       g.fixture_replacement :machinist
