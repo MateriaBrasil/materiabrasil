2d6ca5ea6efad286a1f1b9d9bf5d119086b91909
Luiz Fonseca <runeroniek@gmail.com>
2012-09-09 23:51:11 -0300

    Change environment vars.

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,8 +1,10 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   if Rails.env.development?
-    key, secret     = "372042392867868", "2e40d72fde9afa2ae45096fc5bab517c"
+    key     = "372042392867868"
+    secret  = "2e40d72fde9afa2ae45096fc5bab517c"
   else
-    key, secret     = ENV['KEY_FACEBOOK'], ENV['SECRET_FACEBOOK']
+    key     = ENV['FACEBOOK_KEY']
+    secret  = ENV['FACEBOOK_SECRET']
   end
   provider :facebook, key, secret, scope: "email, user_about_me, user_location"
 end
