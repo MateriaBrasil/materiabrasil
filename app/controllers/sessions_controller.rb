4270c2ab04cb46caee2675920c1b78b0b86f9e32
Luiz Fonseca <runeroniek@gmail.com>
2012-11-13 14:25:40 -0200

    Try to fix error in login/redirect phase.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,15 +2,13 @@ class SessionsController < ApplicationController
 
   def create
     logger.info(auth_hash)
+    logger.info(auth_hash['info'])
+
     unless @auth = Authorization.find_by_provider_and_uid(auth_hash['provider'], auth_hash['uid'])
       @auth = Authorization.create_from_auth_hash(auth_hash)
     end
     self.current_user= @auth.user
-    redirect_to :back and return
-  end
-
-  def new
-    redirect_to explore_path if current_user
+    if params[:redir] then redirect_to explore_path else redirect_to :back and return end
   end
 
   def destroy
