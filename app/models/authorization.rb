c39f587a3947b1793dde4d40ed211b823e0e9b2f
Rafael Barboza <barboza.cardoso@gmail.com>
2013-07-03 00:34:08 -0300

    removes the old auth

--- app/models/authorization.rb
+++ app/models/authorization.rb
@@ -1,5 +1,5 @@
 class Authorization < ActiveRecord::Base
-  
+
   attr_accessible :provider, :user, :uid
   validates_presence_of :provider
   validates_presence_of :uid
@@ -7,9 +7,4 @@ class Authorization < ActiveRecord::Base
 
   belongs_to :user
 
-
-  def self.create_from_auth_hash(hash, user=nil)
-    user ||= User.create_from_auth_hash(hash)
-    Authorization.create!(user: user, uid: hash['uid'], provider: hash['provider'])
-  end
 end
