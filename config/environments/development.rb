896c0a459b6ba1b3b553b110c1ebfebc0908cf36
Rafael Barboza <barboza.cardoso@gmail.com>
2013-07-10 11:03:32 -0300

    adds the email host url config

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -17,6 +17,8 @@ MateriaBrasil::Application.configure do
   # Don't care if the mailer can't send
   config.action_mailer.raise_delivery_errors = false
 
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+
   # Print deprecation notices to the Rails logger
   config.active_support.deprecation = :log
 
@@ -34,5 +36,5 @@ MateriaBrasil::Application.configure do
   config.assets.compress = false
 
   # Expands the lines which load the assets
-  config.assets.debug = false 
+  config.assets.debug = false
 end
