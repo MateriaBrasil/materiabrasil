06b2253fa7f8358f755c5c728663ac12d989fa6b
Gustavo Guichard <gustavoguichard@gmail.com>
2013-07-20 18:41:38 -0300

    Adds responsiveness to header, adds the submit section with a wufoo form

--- config/routes.rb
+++ config/routes.rb
@@ -9,12 +9,12 @@ MateriaBrasil::Application.routes.draw do
   #match "auth/:provider/callback",      to: "sessions#create"
   #resources :sessions, only: [:new, :destroy]
 
-  #get 'auth/facebook',                  as: :facebook_auth
   get 'material/:id',                   to: "materials#show",     as: :material
   get 'explore/:category',              to: "materials#explore",  as: :category
   get 'explore/',                       to: "materials#explore",  as: :explore
   get 'materials',                      to: "materials#index",    as: :materials
   get 'sobre/',                         to: "pages#about",        as: :about
-  get 'busca/',                         to: "materials#search",    as: :search
+  get 'submit/',                        to: "pages#submit",       as: :submit
+  get 'busca/',                         to: "materials#search",   as: :search
   root                                  to: "materials#index"
 end
