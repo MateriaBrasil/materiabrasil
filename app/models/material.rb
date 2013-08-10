40dd4fe00245c2e6acd2e152006ba63ff1259294
Gustavo Guichard <gustavoguichard@gmail.com>
2013-08-10 14:16:45 -0700

    Finished material#show page with characteristics. [Delivers #53830049]

--- app/models/material.rb
+++ app/models/material.rb
@@ -54,11 +54,15 @@ class Material < ActiveRecord::Base
     "#{self.code}-#{self.id.to_s.rjust(5,'0')}"
   end
 
-private
-  def categories_string(pid)
-    categories.map{|c| c.name if c.parent_id == pid}.reject{|c| c.nil?}.join(', ')
+  def categories_string(pid, name = nil)
+    if name
+      categories.map{|c| c.name if c.parent && c.parent.name == name}.reject{|c| c.nil?}.join(', ')
+    elsif pid
+      categories.map{|c| c.name if c.parent_id == pid}.reject{|c| c.nil?}.join(', ')
+    end
   end
 
+private
   def check_tree
     tree = []
     self.categories.each do |cat|
