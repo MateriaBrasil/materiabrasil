c08a40367d3e795151f92d4c1e19510077a46c04
Gustavo Guichard <gustavoguichard@gmail.com>
2013-09-04 13:03:31 -0700

    Added portfolio page with submenu ready and CSS for the menu

--- config/routes.rb
+++ config/routes.rb
@@ -15,6 +15,7 @@ MateriaBrasil::Application.routes.draw do
   get 'materials',                      to: "materials#index",    as: :materials
   get 'sobre/',                         to: "pages#about",        as: :about
   get 'submit/',                        to: "pages#submit",       as: :submit
+  get 'portfolio/',                     to: "pages#portfolio",    as: :portfolio
   get 'busca/',                         to: "materials#search",   as: :search
   root                                  to: "materials#index"
 end
