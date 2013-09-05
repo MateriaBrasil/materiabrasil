941c5d70387e99e72fe771fa0a0735415d99c516
Gustavo Guichard <gustavoguichard@gmail.com>
2013-09-05 12:42:19 -0700

    Portfolio working with JS now

--- app/assets/javascripts/app/views/page.js
+++ app/assets/javascripts/app/views/page.js
@@ -1,6 +1,6 @@
 var Router = Backbone.Router.extend({
   routes : {
-    'i/:param' : 'showPageBlock' 
+    'i/:param' : 'showPageBlock'
   },
 
   showPageBlock: function(param){
@@ -15,6 +15,63 @@ var Router = Backbone.Router.extend({
 });
 
 App.Pages = {
+
+  Portfolio: Backbone.View.extend({
+    el: 'body',
+    events: {
+      'click .thumb_image' : 'activateItem',
+      'click nav.sub_menu a' : 'activateSection'
+    },
+
+    initialize: function(){
+      _.bindAll(this);
+      new Router();
+      Backbone.history.start();
+      this.$portfolio_section = $('.portfolio_section');
+      this.$thumb_links = $('.thumb_links', this.$portfolio_section);
+      this.initializeAllFirstItems();
+    },
+
+    activateSection: function(e){
+      e.preventDefault();
+      $target = $(e.target);
+      $('a', $target.closest('.sub_menu')).removeClass('active');
+      $target.addClass('active');
+      this.showSelectedSection();
+    },
+
+    activateItem: function(e){
+      $target = $(e.target);
+      id = $target.data('item');
+      $('.thumb_image', $target.closest('.thumb_links')).removeClass('active');
+      $target.addClass('active');
+      this.showSelectedItem();
+    },
+
+    initializeAllFirstItems: function(){
+      $('.thumb_image:first', this.$thumb_links).addClass('active')
+      $('a:first', '.sub_menu').addClass('active');
+      this.showSelectedSection();
+      this.showSelectedItem();
+    },
+
+    showSelectedSection: function(){
+      $target = $('a.active', '.sub_menu');
+      anchor = $target.attr('href')
+      $('.portfolio_section', '.page').removeClass('active');
+      $(anchor).addClass('active');
+    },
+
+    showSelectedItem: function(){
+      $('.portfolio_item_art', this.$portfolio_section).removeClass('active');
+      $('.thumb_image.active', this.$thumb_links).each(function(index, item){
+        item_id = $(item).data('item');
+        $item = $('#item_'+item_id, this.$portfolio_section);
+        $item.addClass('active');
+        $('img', $item).hide().fadeIn();
+      })
+    }
+  }),
 
   About: Backbone.View.extend({
     el: 'body',
--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -1,5 +1,2 @@
 module PagesHelper
-  def item_port_thumbnail(item, name)
-    image_tag "portfolio/thumbnails/th_#{item}.jpg", size: "80x80", class: "thumb_image", data: {item: item}
-  end
 end
