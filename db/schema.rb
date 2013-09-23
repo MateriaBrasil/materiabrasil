b0224efc309cb068a0138e9ef6b52ac4618368e3
Rafael Barboza <barboza.cardoso@gmail.com>
2013-09-23 15:21:33 -0300

    Removes the image validation from the material

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130801213245) do
+ActiveRecord::Schema.define(:version => 20130922220047) do
 
   create_table "active_admin_comments", :force => true do |t|
     t.integer  "resource_id",   :null => false
@@ -29,12 +29,12 @@ ActiveRecord::Schema.define(:version => 20130801213245) do
   add_index "active_admin_comments", ["resource_type", "resource_id"], :name => "index_admin_notes_on_resource_type_and_resource_id"
 
   create_table "admin_users", :force => true do |t|
-    t.string   "email",                                 :default => "", :null => false
-    t.string   "encrypted_password",     :limit => 128, :default => "", :null => false
+    t.string   "email",                  :default => "", :null => false
+    t.string   "encrypted_password",     :default => "", :null => false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",                         :default => 0
+    t.integer  "sign_in_count",          :default => 0
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
@@ -89,6 +89,7 @@ ActiveRecord::Schema.define(:version => 20130801213245) do
     t.datetime "created_at",      :null => false
     t.datetime "updated_at",      :null => false
     t.string   "address"
+    t.boolean  "rescrict"
   end
 
   create_table "images", :force => true do |t|
@@ -119,6 +120,11 @@ ActiveRecord::Schema.define(:version => 20130801213245) do
     t.string   "mail_address"
     t.string   "certifications"
     t.integer  "user_id"
+    t.string   "phone"
+    t.string   "image"
+    t.text     "description"
+    t.boolean  "forum"
+    t.string   "forum_mail"
   end
 
   add_index "manufacturers", ["user_id"], :name => "index_manufacturers_on_user_id"
