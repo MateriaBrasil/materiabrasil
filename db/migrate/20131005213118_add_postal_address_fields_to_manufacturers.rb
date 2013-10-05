b2c1f750d3076b16b4bbb81b41d5e86faf9e322c
Rafael Barboza <barboza.cardoso@gmail.com>
2013-10-05 18:46:09 -0300

    adds the new fields to the manufacturer and updates the form

---
+++ db/migrate/20131005213118_add_postal_address_fields_to_manufacturers.rb
@@ -0,0 +1,12 @@
+class AddPostalAddressFieldsToManufacturers < ActiveRecord::Migration
+  def change
+    add_column :manufacturers, :postal_address, :string
+    add_column :manufacturers, :postal_city, :string
+    add_column :manufacturers, :postal_state, :string
+    add_column :manufacturers, :postal_cep, :string
+    add_column :manufacturers, :postal_country, :string
+    add_column :manufacturers, :postal_phone, :string
+    add_column :manufacturers, :has_postal_address, :boolean, null: false, default: false
+    add_column :manufacturers, :forum, :boolean, null: false, default: false
+  end
+end
