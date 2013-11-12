357dcb4a42c3bb48e7690545fe6bce4c32bd01c1
Gustavo Guichard <gustavoguichard@gmail.com>
2013-11-11 17:08:35 -0800

    Fixes some bugs in migrations

--- db/schema.rb
+++ db/schema.rb
@@ -29,12 +29,12 @@ ActiveRecord::Schema.define(:version => 20131005213118) do
   add_index "active_admin_comments", ["resource_type", "resource_id"], :name => "index_admin_notes_on_resource_type_and_resource_id"
 
   create_table "admin_users", :force => true do |t|
-    t.string   "email",                                 :default => "", :null => false
-    t.string   "encrypted_password",     :limit => 128, :default => "", :null => false
+    t.string   "email",                  :default => "", :null => false
+    t.string   "encrypted_password",     :default => "", :null => false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",                         :default => 0
+    t.integer  "sign_in_count",          :default => 0
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
@@ -123,6 +123,7 @@ ActiveRecord::Schema.define(:version => 20131005213118) do
     t.string   "phone"
     t.string   "image"
     t.text     "description"
+    t.boolean  "forum",              :default => false, :null => false
     t.string   "forum_mail"
     t.string   "postal_address"
     t.string   "postal_city"
@@ -131,7 +132,6 @@ ActiveRecord::Schema.define(:version => 20131005213118) do
     t.string   "postal_country"
     t.string   "postal_phone"
     t.boolean  "has_postal_address", :default => false, :null => false
-    t.boolean  "forum",              :default => false, :null => false
   end
 
   add_index "manufacturers", ["user_id"], :name => "index_manufacturers_on_user_id"
@@ -157,13 +157,13 @@ ActiveRecord::Schema.define(:version => 20131005213118) do
   add_index "materials", ["slug"], :name => "index_materials_on_slug", :unique => true
 
   create_table "users", :force => true do |t|
-    t.string   "email",                  :default => "",    :null => false
-    t.datetime "created_at",                                :null => false
-    t.datetime "updated_at",                                :null => false
+    t.string   "email",                  :default => "",   :null => false
+    t.datetime "created_at",                               :null => false
+    t.datetime "updated_at",                               :null => false
     t.string   "name"
     t.string   "city"
     t.string   "state"
-    t.string   "encrypted_password",     :default => "",    :null => false
+    t.string   "encrypted_password",     :default => "",   :null => false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
@@ -172,7 +172,7 @@ ActiveRecord::Schema.define(:version => 20131005213118) do
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
-    t.boolean  "newsletter",             :default => false
+    t.boolean  "newsletter",             :default => true
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
