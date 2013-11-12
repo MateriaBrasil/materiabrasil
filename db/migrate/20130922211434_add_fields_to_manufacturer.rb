18fad5ec33c8622b05aca8a85bd5db4ed1f25176
Gustavo Guichard <gustavoguichard@gmail.com>
2013-11-11 17:08:35 -0800

    Fixes some bugs in migrations

--- db/migrate/20130922211434_add_fields_to_manufacturer.rb
+++ db/migrate/20130922211434_add_fields_to_manufacturer.rb
@@ -3,7 +3,7 @@ class AddFieldsToManufacturer < ActiveRecord::Migration
     add_column :manufacturers, :phone, :string
     add_column :manufacturers, :image, :string
     add_column :manufacturers, :description, :text
-    add_column :manufacturers, :forum, :boolean
+    add_column :manufacturers, :forum, :boolean, null: false, default: false
     add_column :manufacturers, :forum_mail, :string
   end
 end
