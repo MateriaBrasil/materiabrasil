d427003c5a252231b7f2856e210b11167987838e
Rafael Barboza <barboza.cardoso@gmail.com>
2014-02-07 20:49:31 -0200

    adds the policies for the manufacturer

--- app/controllers/manufacturers_controller.rb
+++ app/controllers/manufacturers_controller.rb
@@ -1,7 +1,13 @@
 class ManufacturersController < ApplicationController
   inherit_resources
 
+  def edit
+    authorize resource
+    edit!
+  end
+
   def update
+    authorize resource
     update!(notice: 'Material criado com sucesso!') { root_url }
   end
 end
