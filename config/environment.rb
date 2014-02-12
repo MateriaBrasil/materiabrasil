4d5ff6405e09312125bed0da077bcab36ac6d738
Rafael Barboza <barboza.cardoso@gmail.com>
2014-02-12 20:52:25 -0200

    configures the sendgrid

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,15 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 
+ActionMailer::Base.smtp_settings = {
+  :user_name => ENV['sendgrid_username'],
+  :password => ENV['sendgrid_password'],
+  :domain => 'materiabrasil.com',
+  :address => 'smtp.sendgrid.net',
+  :port => 587,
+  :authentication => :plain,
+  :enable_starttls_auto => true
+}
+
 # Initialize the rails application
 MateriaBrasil::Application.initialize!
