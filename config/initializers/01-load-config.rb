7821e689358b633abadebba672e4071af3c4a02f
Gustavo Guichard <gustavoguichard@gmail.com>
2014-04-30 12:22:38 -0300

    Removed a bug in 01-load-config.rb

--- config/initializers/01-load-config.rb
+++ config/initializers/01-load-config.rb
@@ -1,5 +1,4 @@
-CONFIG = YAML.load_file("#{Rails.root}/config/config.yml")[Rails.env]
-
 unless Rails.env.production?
+  CONFIG = YAML.load_file("#{Rails.root}/config/config.yml")[Rails.env]
   ENV['ADMIN_EMAIL'] = CONFIG['ADMIN_EMAIL']
 end
