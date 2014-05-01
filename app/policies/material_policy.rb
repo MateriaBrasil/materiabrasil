4a03586635bcd76e1eef54ab3d09f749fd3525d3
Rafael Barboza <barboza.cardoso@gmail.com>
2014-05-01 12:51:15 -0300

    Fix the policies

--- app/policies/material_policy.rb
+++ app/policies/material_policy.rb
@@ -14,26 +14,26 @@ class MaterialPolicy < ApplicationPolicy
   end
 
   def edit?
-    user == @material.manufacturer.user || user.email = ENV['ADMIN_EMAIL']
+    user.present? && (user == @material.manufacturer.user || user.email = ENV['ADMIN_EMAIL'])
   end
 
   def edit_attachments?
-    user == @material.manufacturer.user || user.email = ENV['ADMIN_EMAIL']
+    user.present? && (user == @material.manufacturer.user || user.email = ENV['ADMIN_EMAIL'])
   end
 
   def update?
-    user == @material.manufacturer.user || user.email = ENV['ADMIN_EMAIL']
+    user.present? && (user == @material.manufacturer.user || user.email = ENV['ADMIN_EMAIL'])
   end
 
   def update_categories?
-    user == @material.manufacturer.user || user.email = ENV['ADMIN_EMAIL']
+    user.present? && (user == @material.manufacturer.user || user.email = ENV['ADMIN_EMAIL'])
   end
 
   def update_attachments?
-    user == @material.manufacturer.user || user.email = ENV['ADMIN_EMAIL']
+    user.present? && (user == @material.manufacturer.user || user.email = ENV['ADMIN_EMAIL'])
   end
 
   def show?
-    @material.published? || ((@material.draft && @material.user == user) || user.email = ENV['ADMIN_EMAIL'])
+    @material.published? || ((@material.draft && (@material.user == user && user.present?) || user.email = ENV['ADMIN_EMAIL'])
   end
 end
